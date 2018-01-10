105485379b5d0c812842ba0c3160e0e114248ef4
Bjorn <bjossijohnson@gmail.com>
2018-01-10 16:00:36 +0100

    commit

--- code/src/components/page.js
+++ code/src/components/page.js
@@ -1,14 +1,34 @@
 import React from "react"
 import Product from "./product"
-const productsJson = require("./../products.json")
+// const productsJson = require("./../products.json")
+
 
 class Page extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      productsJson: []
+    }
+  }
+
+componentDidMount() {
+  fetch("http://localhost:8080/products").then(response => {
+    return response.json()
+  }).then(json => {
+    console.log("Hello json", json)
+    this.setState({
+      productsJson: json
+    })
+  })
+}
+
+
   render() {
     return (
       <div>
-        <h1>{productsJson.products.length} Produkter</h1>
+        <h1>{this.state.productsJson.length} Produkter</h1>
         <div className="listOfProducts">
-          { productsJson.products.map( (item, index) => {
+          { this.state.productsJson.map( (item, index) => {
             return <Product key={item.id} name={item.name} type={item.type} size={item.size} numberInPack={item.numberInPack} substance={item.substance} price={item.price} deliveryTime={item.deliveryTime} image={item.image} description={item.description}/>
           })}
         </div>
